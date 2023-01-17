0f05a95c74ef83d5bd975b8c72442b0faa06c419
Richa S <69605961+rshar95@users.noreply.github.com>
2023-01-16 23:09:41 -0500

    Create distributed_processing.h
    This code uses MPI to distribute the workload among multiple processes. The master process (rank 0) distributes the workload among the worker processes by iterating through the total amount of work and assigning each iteration to the next process. Each process performs the assigned task and sends the result back to the master process. The master process collects the results from the worker processes and process it.
    
    This is a simple example that demonstrates the basic idea of how to use MPI to distribute the workload among multiple processes. In a real-world application, you may need to adjust this code to handle more complex workloads, such as distributing large data sets or implementing more advanced communication patterns.
    
    It's important to note that in order to run this code you need to have MPI library installed in your system, also you need to use an MPI compiler to build and run the program, such as mpic++.

---
+++ distributed_processing.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // Get the rank and size of the current process
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Distribute the workload among the processes
+    for (int i = rank; i < total_work; i += size) {
+        // Perform some task with the current portion of the workload
+        // Example:
+        // int result = perform_task(i);
+
+        // Send the result back to the master process
+        if (rank != 0) {
+            MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    // Collect the results from the worker processes
+    if (rank == 0) {
+        int result;
+        for (int i = 1; i < size; i++) {
+            MPI_Recv(&result, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // Process the received result
+            // Example:
+            // process_result(result);
+        }
+    }
+
+    // Finalize MPI
+    MPI_Finalize();
+    return 0;
+}
